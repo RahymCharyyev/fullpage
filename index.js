b1bd0f0020a2c89925ce3e7bfdc6d2bcb395f8ac
RahymCharyev <r569837@gmail.com>
2023-02-18 23:49:18 +0500

    ✨feature: test finish

--- index.js
+++
@@ -1,9 +0,0 @@
-new fullpage("#fullpage", {
-  navigation: true,
-  responsiveWidth: 700,
-  anchors: ["home", "about-us", "contact"],
-  parallax: true,
-  onLeave: function (origin, destination, direction) {
-    console.log("Leaving section" + origin.index);
-  },
-});
